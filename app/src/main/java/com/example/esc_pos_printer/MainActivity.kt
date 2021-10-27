16c22e1398c6e00e461ec8b4db7f5c40d4ac45d9
mohamed <mohamed_gsm@msn.com>
2021-10-27 16:45:05 +0200

    initial commit

---
+++ app/src/main/java/com/example/esc_pos_printer/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.esc_pos_printer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
