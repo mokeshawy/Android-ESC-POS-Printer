2526b1a4da16692266db3ee4659960d5c8155568
mohamed <mohamed_gsm@msn.com>
2021-10-28 15:01:17 +0200

    finish implement usb part printer

--- app/src/main/java/com/example/esc_pos_printer/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/esc_pos_printer/fragment/HomeFragment.kt
@@ -1,16 +1,27 @@
 package com.example.esc_pos_printer.fragment
 
 import android.app.AlertDialog
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.content.pm.PackageManager
+import android.hardware.usb.UsbDevice
+import android.hardware.usb.UsbManager
 import android.os.Bundle
+import android.os.Parcelable
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.viewModels
 import com.dantsu.escposprinter.connection.bluetooth.BluetoothConnection
 import com.dantsu.escposprinter.connection.bluetooth.BluetoothPrintersConnections
+import com.dantsu.escposprinter.connection.usb.UsbPrintersConnections
 import com.example.esc_pos_printer.databinding.FragmentHomeBinding
+import com.example.esc_pos_printer.fragment.HomeFragmentViewModel.Companion.ACTION_USB_PERMISSION
 import com.example.esc_pos_printer.fragment.HomeFragmentViewModel.Companion.PERMISSION_BLUETOOTH
 
 
@@ -41,9 +52,50 @@ class HomeFragment : Fragment() {
         binding.btnBluetooth.setOnClickListener {
             homeFragmentViewModel.printBluetooth(requireActivity())
         }
+
+        /* ------ btn print with USB -------- */
+        binding.btnUsb.setOnClickListener {
+            printUsb()
+        }
     }
 
 
+    /**
+     * ------------------- USB Part ----------------
+     */
+
+    private fun printUsb(){
+        val usbConnection = UsbPrintersConnections.selectFirstConnected(requireActivity())
+        val usbManager = requireActivity().getSystemService(Context.USB_SERVICE) as UsbManager
+        if(usbConnection == null || usbManager == null ){
+            val dialog = AlertDialog.Builder(requireActivity())
+            dialog.setTitle("USB Connection")
+            dialog.setMessage("No USB printer found.")
+            dialog.show()
+            return
+        }
+        val permissionIntent = PendingIntent.getBroadcast(requireActivity(),0,Intent(ACTION_USB_PERMISSION),0)
+        val filter = IntentFilter(ACTION_USB_PERMISSION)
+        requireActivity().registerReceiver(usbReceiver,filter)
+        usbManager.requestPermission(usbConnection.device,permissionIntent)
+    }
+
+    private val usbReceiver = object : BroadcastReceiver(){
+        override fun onReceive(context : Context?, intent : Intent?) {
+            val action = intent!!.action
+            if(ACTION_USB_PERMISSION == action){
+                synchronized(requireActivity()){
+                    val usbManager = requireActivity().getSystemService(Context.USB_SERVICE) as UsbManager?
+                    val usbDevice = intent.getParcelableExtra<Parcelable>(UsbManager.EXTRA_DEVICE) as UsbDevice?
+                    if(intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)){
+                        if(usbManager !=null && usbDevice != null ){
+                            homeFragmentViewModel.printUsbPart(requireActivity() , usbManager , usbDevice)
+                        }
+                    }
+                }
+            }
+        }
+    }
 
     /**
      * ------------ BLUETOOTH PART -------------- *
--- app/src/main/java/com/example/esc_pos_printer/fragment/HomeFragmentViewModel.kt
+++ app/src/main/java/com/example/esc_pos_printer/fragment/HomeFragmentViewModel.kt
@@ -4,6 +4,9 @@ import android.app.Activity
 import android.app.AlertDialog
 import android.content.Context
 import android.content.pm.PackageManager
+import android.hardware.usb.UsbConstants
+import android.hardware.usb.UsbDevice
+import android.hardware.usb.UsbManager
 import android.util.DisplayMetrics
 import android.widget.Button
 import androidx.core.app.ActivityCompat
@@ -12,6 +15,8 @@ import androidx.lifecycle.ViewModel
 import com.dantsu.escposprinter.EscPosPrinter
 import com.dantsu.escposprinter.connection.bluetooth.BluetoothConnection
 import com.dantsu.escposprinter.connection.bluetooth.BluetoothPrintersConnections
+import com.dantsu.escposprinter.connection.tcp.TcpConnection
+import com.dantsu.escposprinter.connection.usb.UsbConnection
 import com.dantsu.escposprinter.exceptions.EscPosBarcodeException
 import com.dantsu.escposprinter.exceptions.EscPosConnectionException
 import com.dantsu.escposprinter.exceptions.EscPosEncodingException
@@ -23,15 +28,19 @@ import java.util.*
 
 class HomeFragmentViewModel : ViewModel() {
 
+    companion object{
+        /* ----- bluetooth permission number ----- */
+        var PERMISSION_BLUETOOTH = 1
+
+        /* ------ usb permission ----*/
+        var ACTION_USB_PERMISSION = "com.android.example.USB_PERMISSION"
+    }
 
 
     /**
      * ------------ BLUETOOTH PART -------------- *
      */
-    var selectDevice : BluetoothConnection? = null
-    companion object{
-        var PERMISSION_BLUETOOTH = 1
-    }
+    private var selectDevice : BluetoothConnection? = null
 
     /* ------------- select printer device --------- */
     fun browseBluetoothDevice( context: Context , btnBluetoothBrowsPrinter : Button ) {
@@ -92,15 +101,162 @@ class HomeFragmentViewModel : ViewModel() {
                             "[L]\n" +
                             "[C]================================\n" +
                             "[L]\n" +
-                            "[L]<u><font color='bg-black' size='tall'>Customer :</font></u>\n" +
-                            "[L]Raymond DUPONT\n" +
-                            "[L]5 rue des girafes\n" +
-                            "[L]31547 PERPETES\n" +
-                            "[L]Tel : +33801201456\n" +
+                            "[C]<font size='tall'>Customer Info</font>\n" +
+                            "[L] M KESHAWY\n" +
+                            "[L] 14 Streets\n" +
+                            "[L] Cairo, Egypt\n" +
+                            "[L] Tel : +201007169654\n" +
                             "\n" +
                             "[C]<barcode type='ean13' height='10'>831254784551</barcode>\n" +
                             "[L]\n" +
-                            "[C]<qrcode size='20'>http://www.developpeur-web.dantsu.com/</qrcode>\n"
+                            "[C]<qrcode size='20'>https://github.com/mokeshawy/</qrcode>\n"
+                )
+            }catch ( e: EscPosConnectionException) {
+                e.printStackTrace();
+                AlertDialog.Builder(context)
+                    .setTitle("Broken connection")
+                    .setMessage(e.message)
+                    .show();
+            } catch ( e: EscPosParserException) {
+                e.printStackTrace();
+                AlertDialog.Builder(context)
+                    .setTitle("Invalid formatted text")
+                    .setMessage(e.message)
+                    .show();
+            } catch (e: EscPosEncodingException) {
+                e.printStackTrace();
+                AlertDialog.Builder(context)
+                    .setTitle("Bad selected encoding")
+                    .setMessage(e.message)
+                    .show();
+            } catch (e: EscPosBarcodeException) {
+                e.printStackTrace();
+                AlertDialog.Builder(context)
+                    .setTitle("Invalid barcode")
+                    .setMessage(e.message)
+                    .show();
+            }
+        }
+    }
+
+
+
+    /**
+     * ------------------- USB Part ----------------
+     */
+    fun printUsbPart( context: Context , usbManager: UsbManager , usbDevice: UsbDevice) {
+        if(ContextCompat.checkSelfPermission(context,android.Manifest.permission.BLUETOOTH) != PackageManager.PERMISSION_GRANTED ){
+            ActivityCompat.requestPermissions(context as Activity, arrayOf(android.Manifest.permission.BLUETOOTH),PERMISSION_BLUETOOTH)
+        }else{
+            val format = SimpleDateFormat("'on' yyyy-MM-dd 'at' HH:mm:ss")
+            var printer : EscPosPrinter? = null
+            try {
+
+                printer = EscPosPrinter(UsbConnection(usbManager, usbDevice),203, 48f, 32)
+                @Suppress("DEPRECATION")
+                printer.printFormattedTextAndCut(
+                    "[C]<img>" + PrinterTextParserImg.bitmapToHexadecimalString(printer, context.resources.getDrawableForDensity(
+                        R.drawable.github_logo, DisplayMetrics.DENSITY_MEDIUM)) + "</img>\n" +
+                            "[L]\n" +
+                            "[C]<u><font size='big'>Mohamed Keshawy</font></u>\n" +
+                            "[L]\n" +
+                            "[C]<u type='double'>" + format.format(Date()) + "</u>\n" +
+                            "[C]\n" +
+                            "[C]================================\n" +
+                            "[L]\n" +
+                            "[L]<b>BEAUTIFUL SHIRT</b>[R]9.99€\n" +
+                            "[L]  + Size : S\n" +
+                            "[L]\n" +
+                            "[L]<b>AWESOME HAT</b>[R]24.99€\n" +
+                            "[L]  + Size : 57/58\n" +
+                            "[L]\n" +
+                            "[C]--------------------------------\n" +
+                            "[R]TOTAL PRICE :[R]34.98€\n" +
+                            "[R]TAX :[R]4.23€\n" +
+                            "[L]\n" +
+                            "[C]================================\n" +
+                            "[L]\n" +
+                            "[C]<font size='tall'>Customer Info</font>\n" +
+                            "[L] M KESHAWY\n" +
+                            "[L] 14 Streets\n" +
+                            "[L] Cairo, Egypt\n" +
+                            "[L] Tel : +201007169654\n" +
+                            "\n" +
+                            "[C]<barcode type='ean13' height='10'>831254784551</barcode>\n" +
+                            "[L]\n" +
+                            "[C]<qrcode size='20'>https://github.com/mokeshawy/</qrcode>\n"
+                )
+            }catch ( e: EscPosConnectionException) {
+                e.printStackTrace();
+                AlertDialog.Builder(context)
+                    .setTitle("Broken connection")
+                    .setMessage(e.message)
+                    .show();
+            } catch ( e: EscPosParserException) {
+                e.printStackTrace();
+                AlertDialog.Builder(context)
+                    .setTitle("Invalid formatted text")
+                    .setMessage(e.message)
+                    .show();
+            } catch (e: EscPosEncodingException) {
+                e.printStackTrace();
+                AlertDialog.Builder(context)
+                    .setTitle("Bad selected encoding")
+                    .setMessage(e.message)
+                    .show();
+            } catch (e: EscPosBarcodeException) {
+                e.printStackTrace();
+                AlertDialog.Builder(context)
+                    .setTitle("Invalid barcode")
+                    .setMessage(e.message)
+                    .show();
+            }
+        }
+    }
+
+    /**
+     * ------------------- WIFI Part ----------------
+     */
+    fun printWifiPart( context: Context , ip : String , port : Int ) {
+        if(ContextCompat.checkSelfPermission(context,android.Manifest.permission.BLUETOOTH) != PackageManager.PERMISSION_GRANTED ){
+            ActivityCompat.requestPermissions(context as Activity, arrayOf(android.Manifest.permission.BLUETOOTH),PERMISSION_BLUETOOTH)
+        }else{
+            val format = SimpleDateFormat("'on' yyyy-MM-dd 'at' HH:mm:ss")
+
+            try {
+               val printer = EscPosPrinter(TcpConnection(ip,port),203, 48f, 32)
+                @Suppress("DEPRECATION")
+                printer.printFormattedTextAndCut(
+                    "[C]<img>" + PrinterTextParserImg.bitmapToHexadecimalString(printer, context.resources.getDrawableForDensity(
+                        R.drawable.github_logo, DisplayMetrics.DENSITY_MEDIUM)) + "</img>\n" +
+                            "[L]\n" +
+                            "[C]<u><font size='big'>Mohamed Keshawy</font></u>\n" +
+                            "[L]\n" +
+                            "[C]<u type='double'>" + format.format(Date()) + "</u>\n" +
+                            "[C]\n" +
+                            "[C]================================\n" +
+                            "[L]\n" +
+                            "[L]<b>BEAUTIFUL SHIRT</b>[R]9.99€\n" +
+                            "[L]  + Size : S\n" +
+                            "[L]\n" +
+                            "[L]<b>AWESOME HAT</b>[R]24.99€\n" +
+                            "[L]  + Size : 57/58\n" +
+                            "[L]\n" +
+                            "[C]--------------------------------\n" +
+                            "[R]TOTAL PRICE :[R]34.98€\n" +
+                            "[R]TAX :[R]4.23€\n" +
+                            "[L]\n" +
+                            "[C]================================\n" +
+                            "[L]\n" +
+                            "[C]<font size='tall'>Customer Info</font>\n" +
+                            "[L] M KESHAWY\n" +
+                            "[L] 14 Streets\n" +
+                            "[L] Cairo, Egypt\n" +
+                            "[L] Tel : +201007169654\n" +
+                            "\n" +
+                            "[C]<barcode type='ean13' height='10'>831254784551</barcode>\n" +
+                            "[L]\n" +
+                            "[C]<qrcode size='20'>https://github.com/mokeshawy/</qrcode>\n"
                 )
             }catch ( e: EscPosConnectionException) {
                 e.printStackTrace();
